2fe2e0b5bbf27a73e9511af89f666d8d2caed1d6
Student <student@Mac-mini.local>
2015-10-06 11:11:20 -0600

    more images

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ angular.module('myApp',[])
     .controller("mainController", ["$scope", function($scope) {
 
 
-$scope.picture = ["images/girl.gif", "images/ass.gif"]
+$scope.picture = ["images/girl.gif", "images/ass.gif", "images/breath.gif", "images/tumb.gif", "images/wake.gif"]
 
 $scope.pictures = function(event) {
 	$scope.r = $scope.picture[Math.floor(Math.random()* $scope.picture.length )]
